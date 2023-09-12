8e87b4cf96b3718bd57c8ab0395d2b9045152e1f
ortizyami_idk <L19430151@piedrasnegras.tecnm.mx>
2023-09-12 12:28:16 -0500

    cursosview

---
+++ Models/CursoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_LoginForm.Models
+{
+    public class CursoModel
+    {
+        public string num { get; set; }
+        public string nombre { get; set; }
+        public string area { get; set; }
+        public string tipo { get; set; }
+        public string instructor { get; set; }
+    }
+}
