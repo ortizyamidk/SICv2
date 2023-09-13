a5507822e50f64b19336627d9df35dc5bef41cd2
ortizyami_idk <L19430151@piedrasnegras.tecnm.mx>
2023-09-13 07:42:45 -0500

    antes de menu colapsable

--- Views/CursosView.xaml.cs
+++ Views/CursosView.xaml.cs
@@ -29,38 +29,40 @@ namespace WPF_LoginForm.Views
             ObservableCollection<Curso> cursos = new ObservableCollection<Curso>();
 
             //create datagrid items info
-            cursos.Add(new Curso { num = "1", nombre = "Curso 1", area = "Area 1", tipo = "Interno", instructor = "No definido" });
-            cursos.Add(new Curso { num = "2", nombre = "Curso 2", area = "Area 2", tipo = "Interno", instructor = "No definido" });
-            cursos.Add(new Curso { num = "3", nombre = "Curso 3", area = "Area 3", tipo = "Interno", instructor = "No definido" });
-            cursos.Add(new Curso { num = "4", nombre = "Curso 4", area = "Area 4", tipo = "Externo", instructor = "No definido" });
-            cursos.Add(new Curso { num = "5", nombre = "Curso 5", area = "Area 5", tipo = "Externo", instructor = "No definido" });
-            cursos.Add(new Curso { num = "6", nombre = "Curso 6", area = "Area 6", tipo = "Interno", instructor = "No definido" });
-            cursos.Add(new Curso { num = "7", nombre = "Curso 7", area = "Area 7", tipo = "Interno", instructor = "No definido" });
-            cursos.Add(new Curso { num = "8", nombre = "Curso 8", area = "Area 8", tipo = "Externo", instructor = "No definido" });
-            cursos.Add(new Curso { num = "9", nombre = "Curso 9", area = "Area 9", tipo = "Externo", instructor = "No definido" });
-            cursos.Add(new Curso { num = "10", nombre = "Curso 10", area = "Area 10", tipo = "Externo", instructor = "No definido" });
+            cursos.Add(new Curso { num = "1", nombre = "Curso 1", area = "Area 1", instructor = "No definido" });
+            cursos.Add(new Curso { num = "2", nombre = "Curso 2", area = "Area 2", instructor = "No definido" });
+            cursos.Add(new Curso { num = "3", nombre = "Curso 3", area = "Area 3", instructor = "No definido" });
+            cursos.Add(new Curso { num = "4", nombre = "Curso 4", area = "Area 4", instructor = "No definido" });
+            cursos.Add(new Curso { num = "5", nombre = "Curso 5", area = "Area 5", instructor = "No definido" });
+            cursos.Add(new Curso { num = "6", nombre = "Curso 6", area = "Area 6", instructor = "No definido" });
+            cursos.Add(new Curso { num = "7", nombre = "Curso 7", area = "Area 7", instructor = "No definido" });
+            cursos.Add(new Curso { num = "8", nombre = "Curso 8", area = "Area 8", instructor = "No definido" });
+            cursos.Add(new Curso { num = "9", nombre = "Curso 9", area = "Area 9", instructor = "No definido" });
+            cursos.Add(new Curso { num = "10", nombre = "Curso 10", area = "Area 10", instructor = "No definido" });
 
-            cursos.Add(new Curso { num = "1", nombre = "Curso 1", area = "Area 1", tipo = "Interno", instructor = "No definido" });
-            cursos.Add(new Curso { num = "2", nombre = "Curso 2", area = "Area 2", tipo = "Interno", instructor = "No definido" });
-            cursos.Add(new Curso { num = "3", nombre = "Curso 3", area = "Area 3", tipo = "Interno", instructor = "No definido" });
-            cursos.Add(new Curso { num = "4", nombre = "Curso 4", area = "Area 4", tipo = "Externo", instructor = "No definido" });
-            cursos.Add(new Curso { num = "5", nombre = "Curso 5", area = "Area 5", tipo = "Externo", instructor = "No definido" });
-            cursos.Add(new Curso { num = "6", nombre = "Curso 6", area = "Area 6", tipo = "Interno", instructor = "No definido" });
-            cursos.Add(new Curso { num = "7", nombre = "Curso 7", area = "Area 7", tipo = "Interno", instructor = "No definido" });
-            cursos.Add(new Curso { num = "8", nombre = "Curso 8", area = "Area 8", tipo = "Externo", instructor = "No definido" });
-            cursos.Add(new Curso { num = "9", nombre = "Curso 9", area = "Area 9", tipo = "Externo", instructor = "No definido" });
-            cursos.Add(new Curso { num = "10", nombre = "Curso 10", area = "Area 10", tipo = "Externo", instructor = "No definido" });
+            cursos.Add(new Curso { num = "1", nombre = "Curso 1", area = "Area 1", instructor = "No definido" });
+            cursos.Add(new Curso { num = "2", nombre = "Curso 2", area = "Area 2", instructor = "No definido" });
+            cursos.Add(new Curso { num = "3", nombre = "Curso 3", area = "Area 3", instructor = "No definido" });
+            cursos.Add(new Curso { num = "4", nombre = "Curso 4", area = "Area 4", instructor = "No definido" });
+            cursos.Add(new Curso { num = "5", nombre = "Curso 5", area = "Area 5", instructor = "No definido" });
+            cursos.Add(new Curso { num = "6", nombre = "Curso 6", area = "Area 6", instructor = "No definido" });
+            cursos.Add(new Curso { num = "7", nombre = "Curso 7", area = "Area 7", instructor = "No definido" });
+            cursos.Add(new Curso { num = "8", nombre = "Curso 8", area = "Area 8", instructor = "No definido" });
+            cursos.Add(new Curso { num = "9", nombre = "Curso 9", area = "Area 9", instructor = "No definido" });
+            cursos.Add(new Curso { num = "10", nombre = "Curso 10", area = "Area 10", instructor = "No definido" });
 
-            cursos.Add(new Curso { num = "1", nombre = "Curso 1", area = "Area 1", tipo = "Interno", instructor = "No definido" });
-            cursos.Add(new Curso { num = "2", nombre = "Curso 2", area = "Area 2", tipo = "Interno", instructor = "No definido" });
-            cursos.Add(new Curso { num = "3", nombre = "Curso 3", area = "Area 3", tipo = "Interno", instructor = "No definido" });
-            cursos.Add(new Curso { num = "4", nombre = "Curso 4", area = "Area 4", tipo = "Externo", instructor = "No definido" });
-            cursos.Add(new Curso { num = "5", nombre = "Curso 5", area = "Area 5", tipo = "Externo", instructor = "No definido" });
-            cursos.Add(new Curso { num = "6", nombre = "Curso 6", area = "Area 6", tipo = "Interno", instructor = "No definido" });
-            cursos.Add(new Curso { num = "7", nombre = "Curso 7", area = "Area 7", tipo = "Interno", instructor = "No definido" });
-            cursos.Add(new Curso { num = "8", nombre = "Curso 8", area = "Area 8", tipo = "Externo", instructor = "No definido" });
-            cursos.Add(new Curso { num = "9", nombre = "Curso 9", area = "Area 9", tipo = "Externo", instructor = "No definido" });
-            cursos.Add(new Curso { num = "10", nombre = "Curso 10", area = "Area 10", tipo = "Externo", instructor = "No definido" });
+            cursos.Add(new Curso { num = "1", nombre = "Curso 1", area = "Area 1", instructor = "No definido" });
+            cursos.Add(new Curso { num = "2", nombre = "Curso 2", area = "Area 2", instructor = "No definido" });
+            cursos.Add(new Curso { num = "3", nombre = "Curso 3", area = "Area 3", instructor = "No definido" });
+            cursos.Add(new Curso { num = "4", nombre = "Curso 4", area = "Area 4", instructor = "No definido" });
+            cursos.Add(new Curso { num = "5", nombre = "Curso 5", area = "Area 5", instructor = "No definido" });
+            cursos.Add(new Curso { num = "6", nombre = "Curso 6", area = "Area 6", instructor = "No definido" });
+            cursos.Add(new Curso { num = "7", nombre = "Curso 7", area = "Area 7", instructor = "No definido" });
+            cursos.Add(new Curso { num = "8", nombre = "Curso 8", area = "Area 8", instructor = "No definido" });
+            cursos.Add(new Curso { num = "9", nombre = "Curso 9", area = "Area 9", instructor = "No definido" });
+            cursos.Add(new Curso { num = "10", nombre = "Curso 10", area = "Area 10", instructor = "No definido" });
+
+
 
             cursosDataGrid.ItemsSource = cursos;
 
@@ -72,7 +74,6 @@ namespace WPF_LoginForm.Views
             public string num { get; set; }
             public string nombre { get; set; }
             public string area { get; set; }
-            public string tipo { get; set; }
             public string instructor { get; set; }
         }
     }
