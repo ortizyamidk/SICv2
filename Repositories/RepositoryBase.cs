8b2a56811ac55e0dbf59cc0c96df6dc4a1e35824
ortizyami_idk <L19430151@piedrasnegras.tecnm.mx>
2023-12-11 13:19:41 -0600

    fin

--- Repositories/RepositoryBase.cs
+++ Repositories/RepositoryBase.cs
@@ -7,7 +7,7 @@ namespace WPF_LoginForm.Repositories
         private readonly string _connectionString;
         public RepositoryBase()
         {
-            _connectionString = "Server=ORTIZYAMIIDK\\SQLEXPRESS; Database=sicdb; User Id=sa; Password=1234;";
+            _connectionString = "Server=ORTIZYAMIIDK\\SQLEXPRESS; Database=sicdbfinal; User Id=sa; Password=1234;";
         }
         protected SqlConnection GetConnection()
         {
